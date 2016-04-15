36131709cb13876668d1389c1623d8bfe18e1852
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-04-15 14:19:47 +0100

    Fleshed out sprint 1 journeys

--- app/routes/sprint1.js
+++ app/routes/sprint1.js
@@ -5,24 +5,52 @@ router.get('/start', function (req, res) {
   res.render('sprint1/start-page.html');
 });
 
+router.get('/preq-relationship-status', function (req, res) {
+  res.render('sprint1/relationship-status.html');
+});
+
+router.post('/preq-relationship-status', function (req, res) {
+  res.redirect('preq-date');
+});
+
 router.get('/preq-date', function (req, res) {
   res.render('sprint1/what-date-did-they-die.html');
 });
 
-router.get('/preq-marriage', function (req, res) {
-  res.render('sprint1/were-you-still-married.html');
+router.post('/preq-date', function (req, res) {
+  res.redirect('preq-did-you-live-in-the-uk');
 });
 
-router.get('/preq-location', function (req, res) {
+router.get('/preq-did-you-live-in-the-uk', function (req, res) {
   res.render('sprint1/did-you-live-in-the-uk.html');
+});
+
+router.post('/preq-did-you-live-in-the-uk', function (req, res) {
+  res.redirect('details');
 });
 
 router.get('/details', function (req, res) {
   res.render('sprint1/details.html');
 });
 
+router.post('/details', function (req, res) {
+  res.redirect('details-partner');
+});
+
+router.get('/details-partner', function (req, res) {
+  res.render('sprint1/details-partner.html');
+});
+
+router.post('/details-partner', function (req, res) {
+  res.redirect('dependant-children');
+});
+
 router.get('/dependant-children', function (req, res) {
   res.render('sprint1/dependant-children.html');
+});
+
+router.post('/dependant-children', function (req, res) {
+  res.redirect('bank-details');
 });
 
 // child benefit?
@@ -31,8 +59,16 @@ router.get('/bank-details', function (req, res) {
   res.render('sprint1/bank-details.html');
 });
 
+router.post('/bank-details', function (req, res) {
+  res.redirect('contact');
+});
+
 router.get('/contact', function (req, res) {
   res.render('sprint1/contact.html');
+});
+
+router.post('/contact', function (req, res) {
+  res.redirect('end');
 });
 
 router.get('/end', function (req, res) {
