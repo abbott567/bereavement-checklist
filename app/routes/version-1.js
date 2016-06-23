edf8657b2483cef56c0e2825cb331782b54bbec4
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-06-23 11:15:40 +0100

    Added checklist

--- app/routes/version-1.js
+++ app/routes/version-1.js
@@ -2,13 +2,19 @@
 
 const express = require('express');
 const router = new express.Router();
+const moment = require('moment');
 
 router.get('/', (req, res) => {
   res.redirect('/version-1/start');
 });
 
 router.get('/start', (req, res) => {
-  res.render('version-1/start-page.html');
+  const date = {};
+  date.twoWeeks = moment().day(14).format('D MMMM YYYY');
+  date.threeMonths = moment().add(3, 'months').format('D MMMM YYYY');
+  date.sixMonths = moment().add(6, 'months').format('D MMMM YYYY');
+
+  res.render('version-1/start-page.html', {date});
 });
 
 module.exports = router;
