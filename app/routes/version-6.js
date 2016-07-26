bf5e037ec3e3ecb50bde4ce84ebb4b7d893b7d2e
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-07-26 11:13:18 +0100

    Updated based on UR analysis

--- app/routes/version-6.js
+++ app/routes/version-6.js
@@ -6,12 +6,12 @@ const router = new express.Router();
 
 // Redirect to start page
 router.get('/', (req, res) => {
-  res.redirect('/version-5/start');
+  res.redirect('/version-6/start');
 });
 
 // Start page
 router.get('/start', (req, res) => {
-  res.render('version-5/start-page.html');
+  res.render('version-6/start-page.html');
 });
 
 router.post('/start', (req, res) => {
@@ -20,7 +20,7 @@ router.post('/start', (req, res) => {
 
 // About the deceased person
 router.get('/details-partner', (req, res) => {
-  res.render('version-5/details-partner.html');
+  res.render('version-6/details-partner.html');
 });
 
 router.post('/details-partner', (req, res) => {
@@ -31,7 +31,7 @@ router.post('/details-partner', (req, res) => {
 // About you
 router.get('/details-yours', (req, res) => {
   const name = req.cookies.deceasedName;
-  res.render('version-5/details-yours.html', {name});
+  res.render('version-6/details-yours.html', {name});
 });
 
 router.post('/details-yours', (req, res) => {
@@ -60,7 +60,7 @@ router.post('/details-yours', (req, res) => {
 
 // Your contact details
 router.get('/details-contact', (req, res) => {
-  res.render('version-5/details-contact.html');
+  res.render('version-6/details-contact.html');
 });
 
 router.post('/details-contact', (req, res) => {
@@ -70,7 +70,7 @@ router.post('/details-contact', (req, res) => {
 // Next of kin details
 router.get('/details-next-of-kin', (req, res) => {
   const name = req.cookies.deceasedName;
-  res.render('version-5/details-next-of-kin.html', {name});
+  res.render('version-6/details-next-of-kin.html', {name});
 });
 
 router.post('/details-next-of-kin', (req, res) => {
@@ -80,7 +80,7 @@ router.post('/details-next-of-kin', (req, res) => {
 // Inform organisations about the death
 router.get('/inform-organisations', (req, res) => {
   const name = req.cookies.deceasedName;
-  res.render('version-5/inform-organisations.html', {name});
+  res.render('version-6/inform-organisations.html', {name});
 });
 
 router.post('/inform-organisations', (req, res) => {
@@ -94,75 +94,40 @@ router.get('/whats-been-done', (req, res) => {
   date.ten = moment().add(10, 'days').format('DD MMMM YYYY');
   date.fourteen = moment().add(14, 'days').format('DD MMMM YYYY');
 
-  res.render('version-5/whats-been-done.html', {name, date});
+  res.render('version-6/whats-been-done.html', {name, date});
 });
 
 router.post('/whats-been-done', (req, res) => {
-  // res.redirect('checklist');\n  // res.redirect('checklist');
-  res.redirect('checklist-with-boxes');
+  res.redirect('things-to-think-about');
 });
 
 // Checklist
-// router.get('/checklist', (req, res) => {
-//   let checklist = req.cookies.checklist;
-//   const bspElig = req.cookies.bspElig;
-//   const name = req.cookies.deceasedName;
-//   if (checklist === undefined) {
-//     checklist = {
-//       bsp: 'No',
-//       registered: 'No',
-//       funeral: 'No',
-//       bankAccounts: 'No',
-//       insurance: 'No',
-//       employers: 'No',
-//       funeralSupport: 'No',
-//       passport: 'No',
-//       drivinglicence: 'No',
-//       probate: 'No'
-//     };
-//     res.cookie('checklist', checklist);
-//   }
-//
-//   res.render('version-5/checklist.html', {checklist, bspElig, name});
-// });
-
-// Checklist
-router.get('/checklist-with-boxes', (req, res) => {
-  let checklist = req.cookies.checklist;
+router.get('/things-to-think-about', (req, res) => {
+  const completeDate = moment().format('DD MMMM YYYY');
   const bspElig = req.cookies.bspElig;
   const name = req.cookies.deceasedName;
-  if (checklist === undefined) {
-    checklist = {
-      bsp: 'No',
-      registered: 'No',
-      funeral: 'No',
-      bankAccounts: 'No',
-      insurance: 'No',
-      employers: 'No',
-      funeralSupport: 'No',
-      passport: 'No',
-      drivinglicence: 'No',
-      probate: 'No'
-    };
-    res.cookie('checklist', checklist);
+  const randomDates = [];
+
+  for (let i = 0; i < 10; i++) {
+    randomDates.push(moment().add(randomNum(14, 30), 'days').format('DD MMMM YYYY'));
   }
 
-  res.render('version-5/checklist-with-boxes.html', {checklist, bspElig, name});
+  res.render('version-6/things-to-think-about.html', {bspElig, name, completeDate, randomDates});
 });
 
 router.get('/find-a-funeral-director', (req, res) => {
   const backLink = 'have-you-arranged-funeral';
-  res.render('version-5/find-a-funeral-director.html', {backLink});
+  res.render('version-6/find-a-funeral-director.html', {backLink});
 });
 
 router.post('/find-a-funeral-director', (req, res) => {
   // res.redirect('checklist');
-  res.redirect('checklist-with-boxes');
+  res.redirect('things-to-think-about');
 });
 
 router.get('/apply-for-sffp', (req, res) => {
   const backLink = req.get('referrer').indexOf('funeral-no') > -1 ? 'funeral-no' : 'funeral-date';
-  res.render('version-5/apply-for-sffp.html', {backLink});
+  res.render('version-6/apply-for-sffp.html', {backLink});
 });
 
 router.post('/apply-for-sffp', (req, res) => {
@@ -181,10 +146,10 @@ router.get('/apply-for-bsp', (req, res) => {
   }
 
   if (eligible) {
-    res.render('version-5/apply-for-bsp.html');
+    res.render('version-6/apply-for-bsp.html');
   } else {
     // res.redirect('checklist');
-    res.redirect('checklist-with-boxes');
+    res.redirect('things-to-think-about');
   }
 });
 
@@ -194,13 +159,13 @@ router.post('/apply-for-bsp', (req, res) => {
   } else {
     res.cookie('checklist', true);
     // res.redirect('checklist');
-    res.redirect('checklist-with-boxes');
+    res.redirect('things-to-think-about');
   }
 });
 
 router.get('/bsp-eligibility', (req, res) => {
   const backLink = 'apply-for-bsp';
-  res.render('version-5/bsp-eligibility.html', {backLink});
+  res.render('version-6/bsp-eligibility.html', {backLink});
 });
 
 router.post('/bsp-eligibility', (req, res) => {
@@ -213,12 +178,12 @@ router.post('/bsp-eligibility', (req, res) => {
 
 router.get('/bsp-exit', (req, res) => {
   const backLink = 'bsp-eligibility';
-  res.render('version-5/bsp-exit-page.html', {backLink});
+  res.render('version-6/bsp-exit-page.html', {backLink});
 });
 
 router.get('/bsp-dependent-children', (req, res) => {
   const backLink = 'bsp-eligibility';
-  res.render('version-5/bsp-dependent-children.html', {backLink});
+  res.render('version-6/bsp-dependent-children.html', {backLink});
 });
 
 router.post('/bsp-dependent-children', (req, res) => {
@@ -227,7 +192,7 @@ router.post('/bsp-dependent-children', (req, res) => {
 
 router.get('/bsp-bank-details', (req, res) => {
   const backLink = 'bsp-dependent-children';
-  res.render('version-5/bsp-bank-details.html', {backLink});
+  res.render('version-6/bsp-bank-details.html', {backLink});
 });
 
 router.post('/bsp-bank-details', (req, res) => {
@@ -236,7 +201,7 @@ router.post('/bsp-bank-details', (req, res) => {
 
 router.get('/bsp-declaration', (req, res) => {
   const backLink = 'bsp-bank-details';
-  res.render('version-5/bsp-declaration.html', {backLink});
+  res.render('version-6/bsp-declaration.html', {backLink});
 });
 
 router.post('/bsp-declaration', (req, res) => {
@@ -248,12 +213,12 @@ router.get('/bsp-end', (req, res) => {
   checklist.bsp = 'Yes';
   res.cookie('checklist', checklist);
   const completeDate = moment().format('DD MMMM YYYY');
-  res.render('version-5/bsp-end-page.html', {completeDate});
+  res.render('version-6/bsp-end-page.html', {completeDate});
 });
 
 router.post('/bsp-end', (req, res) => {
   // res.redirect('checklist');
-  res.redirect('checklist-with-boxes');
+  res.redirect('things-to-think-about');
 });
 
 router.get('/mark-as-complete/:id', (req, res) => {
@@ -269,7 +234,11 @@ router.get('/mark-as-complete/:id', (req, res) => {
   }
 
   res.cookie('checklist', checklist);
-  res.redirect('/version-5/checklist');
+  res.redirect('/version-6/checklist');
 });
 
 module.exports = router;
+
+function randomNum(min, max) {
+  return Math.floor((Math.random() * (max - min + 1)) + min);
+}
